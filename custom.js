d6ddde8d0c1aa93e7dae7411039280a91429f1bc
annapetriga33 <annapetriga33@gmail.com>
2021-08-14 11:53:48 +0300

    second commit

--- custom.js
+++ custom.js
@@ -1,7 +1,5 @@
 function isLannisterSoldier(armorColor, shield) {
-    if (armorColor === 'red' && shield === null) {
-        console.log(true);
-    } else if (armorColor === 'red' && shield === 'lion') {
+    if (armorColor === 'red' && shield === null || shield === 'lion') {
         console.log(true);
     } else {
         console.log(false);
